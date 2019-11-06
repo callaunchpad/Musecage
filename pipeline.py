c58eefb7f3d1c79335f12243f71e1a3aba97804a
Arjun Khare <ajnkhare@gmail.com>
2019-11-05 23:46:48 -0800

    added pipeline code

--- pipeline.py
+++ pipeline.py
@@ -8,13 +8,19 @@ from load_create_data import *
 from knn import KNNModel
 
 class Pipeline():
-    def __init__(self, data_arr, metric="min_k"):
+    def __init__(self, data_arr, metric="min_k", batch_size=64, replace=False):
         self.data_arr = data_arr
         self.metric = metric
+        self.batch_size = batch_size
+        self.replace = replace
         
-    def create_split(self, split_val=.8):
-        self.train_data = self.data_arr[:int(len(self.data_arr) * split_val)]
-        self.test_data = self.data_arr[int(len(self.data_arr) * split_val):]
+    def create_split(self, split_val=.8, custom_split=False, custom_train=[], custom_test=[]):
+        if custom_split:
+            self.train_data = custom_train
+            self.test_data = custom_test
+        else:
+            self.train_data = self.data_arr[:int(len(self.data_arr) * split_val)]
+            self.test_data = self.data_arr[int(len(self.data_arr) * split_val):]
 
         self.train_q_arr = [format_q_for_embed(val["question"]) for val in self.train_data]
         self.test_q_arr = [format_q_for_embed(val["question"]) for val in self.test_data]
@@ -28,7 +34,14 @@ class Pipeline():
         self.train_ans_arr = [val["answers"] for val in self.train_data]
         self.test_ans_arr = [val["answers"] for val in self.test_data]
 
-    def get_preds(self, model_class=KNNModel, k=5):
+    def next_train_batch(self):
+        train_q_batch = np.random.choice(self.train_q_arr, self.batch_size, self.replace)
+        train_q_id_batch = np.random.choice(self.train_q_id_arr, self.batch_size, self.replace)
+        train_im_id_batch = np.random.choice(self.train_im_id_arr, self.batch_size, self.replace)
+        train_ans_batch = np.random.choice(self.train_ans_arr, self.batch_size, self.replace)
+        return train_q_batch, train_q_id_batch, train_im_id_batch, train_ans_batch
+
+    def get_preds(self, model_class=KNNModel, k=4):
         model = model_class(k)
         model.train(self.train_q_arr, self.train_q_id_arr, self.train_im_id_arr, self.train_ans_arr)
         
@@ -36,18 +49,21 @@ class Pipeline():
 
     def get_accuracy(self):
         acc = 0
-        for i in len(self.preds):
-            count = self.test_ans_arr[i].count(self.preds[i])
+        for i in range(len(self.preds)):
+            count = self.test_ans_arr[i].count(self.preds[i][0])
+            print(self.preds[i], self.test_ans_arr[i])
             if self.metric == "min_k":
                 acc += min(count/3, 1)    
         acc /= len(self.preds)
         return acc
 
-data_arr = get_by_ques_type(["how many"])
+data_arr = get_by_ques_type(["what is the man"])
+print(len(data_arr))
 p = Pipeline(data_arr)
 p.create_split()
 p.get_preds()
-p.get_accuracy()
+acc = p.get_accuracy()
+print(acc)
 
         
                 
