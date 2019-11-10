a017617d5c8583407d4c14bef5fdacfa668f1b4f
James Jiao <james.j.jiao@gmail.com>
2019-11-10 11:09:00 -0800

    transferred rnn correct to rnn4

--- RNN/rnn4.py
+++ RNN/rnn4.py
@@ -6,30 +6,65 @@ import collections
 
 class RNNModel():
 
-	def __init__ RNNModel(self, num_hidden=512, num_input=10, timesteps=3, vocab_size): 
+	def __init__ RNNModel(self, n_hidden=512, n_input=3, training_file): 
 
 		# Network Parameters
-		self.num_input = num_input
-		self.timesteps = timesteps
-		self.num_hidden = num_hidden # hidden layer num of features
-		self.num_classes = vocab_size #num of unique words
+		self.n_input = n_input
+		self.n_hidden = n_hidden # hidden layer num of features
+
+		def read_data(fname):
+		    with open(fname) as f:
+		        content = f.readlines()
+		    content = [x.strip() for x in content]
+		    content = [word for i in range(len(content)) for word in content[i].split()]
+		    content = np.array(content)
+		    return content
+
+		training_data = read_data(training_file)
+
+		def build_dataset(words):
+		    count = collections.Counter(words).most_common()
+		    dictionary = dict()
+		    for word, _ in count:
+		        dictionary[word] = len(dictionary)
+		    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+		    return dictionary, reverse_dictionary
+
+		dictionary, reverse_dictionary = build_dataset(training_data)
+		vocab_size = len(dictionary)
 
 		self.weights = {
-			'out': tf.Variable(tf.random_normal([num_hidden, vocab_size]))
+			'out': tf.Variable(tf.random_normal([n_hidden, vocab_size]))
 		}
 		self.biases = {
 			'out': tf.Variable(tf.random_normal([vocab_size]))
 		}
 
 		# tf Graph input
-		X = tf.placeholder("float", [None, timesteps, num_input])
-		Y = tf.placeholder("float", [None, num_classes])
+		X = tf.placeholder("float", [None, n_input], 1)
+		Y = tf.placeholder("float", [None, vocab_size])
 
 	def RNN(self, x):
-		x = tf.reshape(x, [-1, self.num_input])
-		x = tf.split(x, self.num_input,1)
-		rnn_cell = rnn.BasicLSTMCell(self.num_hidden)
-		outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
-		return tf.matmul(outputs[-1], self.weights['out']) + self.biases['out']
-		#one hot encode x
+	    # reshape to [1, n_input]
+	    x = tf.reshape(x, [-1, n_input])
+
+	    # Generate a n_input-element sequence of inputs
+	    # (eg. [had] [a] [general] -> [20] [6] [33])
+	    x = tf.split(x,n_input,1)
+
+	    # 2-layer LSTM, each layer has n_hidden units.
+	    # Average Accuracy= 95.20% at 50k iter
+	    rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(n_hidden),rnn.BasicLSTMCell(n_hidden)])
+
+	    # 1-layer LSTM with n_hidden units but with lower accuracy.
+	    # Average Accuracy= 90.60% 50k iter
+	    # Uncomment line below to test but comment out the 2-layer rnn.MultiRNNCell above
+	    # rnn_cell = rnn.BasicLSTMCell(n_hidden)
+
+	    # generate prediction
+	    outputs, states = rnn.static_rnn(rnn_cell, x, dtype=tf.float32)
+
+	    # there are n_input outputs but
+	    # we only want the last output
+	    return tf.matmul(outputs[-1], weights['out']) + biases['out']
 	
