9ce4caaf6f08c40cf3acd8423126c14c37d03481
Reina Wang <reina.wang.tw@gmail.com>
2019-11-13 23:02:01 -0800

    integrated rnn into fcnn

--- RNN/rnn4.py
+++ RNN/rnn4.py
@@ -5,22 +5,20 @@ import numpy as np
 
 class RNNModel():
 
-	def __init__ (self, embedding_len = 1024, n_hidden=512): 
-		"""
-		params: 
-		depth: number of words in all the questions...ever
-		indicies: an array of indices of each word in the question
-		"""
+	def __init__ (self, batched_rnn_input, n_hidden=512): 
+
 		# Network Parameters
 		# num_words = len(indices)
 		# question_embedding = tf.one_hot(indices, depth)
 		# tf Graph input
-		self.x = tf.placeholder(dtype=tf.float32, shape=[None, 1, embedding_len], name="inputs")
+		# self.x = tf.placeholder(dtype=tf.float32, shape=[None, 1, embedding_len], name="inputs")
+		self.n_hidden = n_hidden
+		self.batched_rnn_input = batched_rnn_input
 		self.output = self._build_graph()
 
 	def _build_graph(self):
 		rnn_cell = rnn.MultiRNNCell([rnn.BasicLSTMCell(self.n_hidden),rnn.BasicLSTMCell(self.n_hidden)])
-		outputs, states = tf.nn.dynamic_rnn(rnn_cell, self.x, dtype=tf.float32)
+		outputs, states = tf.nn.dynamic_rnn(rnn_cell, self.batched_rnn_input, dtype=tf.float32)
 
 		print("Output shape:", tf.shape(outputs))
 		print("States shape:", tf.shape(states))
