6c15125221d8e146b44e9ff1fa901c0b66a218c7
Reina Wang <reina.wang.tw@gmail.com>
2019-11-18 16:57:49 -0800

    added class/function descriptions for FCNN.py

--- src/FCNN.py
+++ src/FCNN.py
@@ -4,13 +4,27 @@ import random
 from rnn_model import RNNModel
 
 class FCNN:
-	def __init__(self, cnn_input_size, rnn_input_size, pointwise_layer_size, output_size, vocab_size, 
+	def __init__(self, cnn_input_size, pointwise_layer_size, output_size, vocab_size, 
 					glove_embed_size=300, word2vec_embed_size=300, dropout=0.2, net_struct={'h1': 1000}, 
 			        activation_fn=tf.nn.relu, embed_type="RNN",
 			        loss_fn=tf.nn.sparse_softmax_cross_entropy_with_logits, lr=1e-3):
+		"""
+        Creates FCNNModel based on:
+            - cnn_input_size: dimension of image embedding
+			- pointwise_layer_size: dimension of the pointwise layer
+			- output_size: dimension of the output layer
+            - vocab_size: number of top words the model will choose a solution from
+            - glove_embed_size: dimension of the glove embedding
+			- word2vec_embed_size: dimension of word2vec embedding
+			- dropout: rate of dropout layer
+			- net_struct: dictionary of {name: layer_size} for each layer in the fully connected layers
+			- activation_fn: activation function
+			- embed_type: type of question embedding used; can be "Glove", "Word2Vec", or "RNN"
+			- loss_fn: loss function
+			- lr: learning rate
+        """
 		
 		self.cnn_input_size = cnn_input_size
-		self.rnn_input_size = rnn_input_size
 		self.pointwise_layer_size = pointwise_layer_size
 		self.output_size = output_size
 		self.vocab_size = vocab_size
@@ -26,6 +40,9 @@ class FCNN:
 		self.build_model()
 
 	def build_model(self):
+		"""
+        Builds graph of FCNN, called only during __init__. 
+        """
 		self.cnn_in = tf.placeholder(tf.float64, [None, self.cnn_input_size], name="cnn_input")
 
 		if self.embed_type == "RNN":
@@ -71,14 +88,35 @@ class FCNN:
 		self.train_op = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
 
 	def evaluate(self, sess, cnn_batch, q_batch, label_batch):
+		"""
+        Evaluates loss of the FCNN model with parameters:
+            - sess: a tensorflow session
+			- cnn_batch: a batch of images to evaluate on
+			- q_batch: a batch of questions to evaluate on
+			- label_batch: a batch of labels to evaluate on
+        """
 		loss = sess.run(self.loss, feed_dict={self.cnn_in: cnn_batch, self.q_batch: q_batch, self.labels: label_batch})
 		return loss
 
 	def train_step(self, sess, cnn_batch, q_batch, label_batch):
+		"""
+        Trains the FCNN model with parameters:
+            - sess: a tensorflow session
+			- cnn_batch: a batch of training images
+			- q_batch: a batch of training questions
+			- label_batch: a batch of trainin labels
+        """
 		_, loss = sess.run([self.train_op, self.loss], feed_dict={self.cnn_in: cnn_batch, self.q_batch: q_batch, self.labels: label_batch})
 		return loss
 
 	def get_output(self, sess, cnn_in, q_in, label_in):
+		"""
+        Tests and returns the output of the FCNN model with:
+            - sess: a tensorflow session
+			- cnn_in: a test image
+			- q_in: a test question
+			- label_in: a test label
+        """
 		output = sess.run(self.output, feed_dict={self.cnn_in: cnn_in, self.q_batch: q_in, self.labels: label_in})
 		return output
 
